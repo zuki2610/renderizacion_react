9877dad32c3ffadc56ccecc9aa57c02e520694b3
Isabel Palacios <issabelpalacios@gmail.com>
2023-02-01 20:02:21 -0300

    Initial commit create repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Productos from './Componentes/Produtos';
 import Home from './Componentes/Home';
--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import '../App.css';
+import '../index.css';
 import Header from './Header';
-import Image from 'react-bootstrap/Image';
 import Menu from './Menu';
 import Footer from './Footer';
 
@@ -14,7 +13,6 @@ return (
      <div className="home App bg-dark p-5">
     <div className="marvel">
     <Header titulo="Donde encontrarás los mejores juguetes y accesorios de Marvel Comics para ti"/>
-    <Image className="p-5" src="https://raw.githubusercontent.com/zuki2610/renderizacion_react/main/public/assets/img/Logo1.png"/>
     </div>
     <Footer /> 
     </div>
